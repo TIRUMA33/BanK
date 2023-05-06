49073a1b14a16b17ff0054c842f2c7a3840899c5
pablorgu <pablorg@uma.es>
2023-05-06 23:34:46 +0200

    cambios locos

--- src/main/java/es/uma/taw/bank/service/UsuarioService.java
+++ src/main/java/es/uma/taw/bank/service/UsuarioService.java
@@ -2,6 +2,7 @@ package es.uma.taw.bank.service;
 
 import es.uma.taw.bank.dao.UsuarioRepository;
 import es.uma.taw.bank.dto.UsuarioDTO;
+import es.uma.taw.bank.entity.UsuarioEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,14 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
+    public void guardarUsuario(UsuarioDTO dto){
+        UsuarioEntity usuario;
+        usuario = new UsuarioEntity();
+        usuario.setNif(dto.getNif());
+        usuario.setContrasena(dto.getContrasena());
+        usuario.setId(dto.getId());
+        this.usuarioRepository.save(usuario);
+    }
     public UsuarioDTO buscarUsuario(Integer id) {
         return Objects.requireNonNull(this.usuarioRepository.findById(id).orElse(null)).toDTO();
     }
