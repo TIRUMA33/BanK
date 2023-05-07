a6c4990a4c3bbf26cca54fe810ec9f5896d357d9
pablorgu <pablorg@uma.es>
2023-05-07 23:08:52 +0200

    mas

--- src/main/java/es/uma/taw/bank/dao/EstadoCuentaRepository.java
+++ src/main/java/es/uma/taw/bank/dao/EstadoCuentaRepository.java
@@ -1,5 +1,5 @@
 package es.uma.taw.bank.dao;
-//pablo ruiz galianez
+//Pablo Ruiz Galianez
 import es.uma.taw.bank.entity.EstadoCuentaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
